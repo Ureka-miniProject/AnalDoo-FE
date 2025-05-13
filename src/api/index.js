2a122c7d6140d8f2c3cdf8cab9966fa31989a260
Jinu Yi <dionisos198@naver.com>
2025-05-13 23:46:34 +0900

    refactor:  .env 파일 참조하여 API 요청

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 인스턴스 생성
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.VUE_APP_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
